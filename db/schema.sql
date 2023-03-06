d9906cf47a0e7c0dc0d8b37e440a70029462eebc
Dantallope <dantebraccialini@gmail.com>
2023-03-06 15:59:40 -0700

    initial inquirer prompt

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,13 @@
 drop database if exists employee_db;
 create database employee_db:
 use employee_db;
+
+create table employees (
+  id int AUTO_INCREMENT not null,
+  firstName varchar(100) not null,
+  lastName varchar(100) not null,
+  title varchar(100) not null,
+  department varchar(100) not null,
+  salary int not null,
+  manager (varchar)
+)
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 const inquirer = require('inquirer');
-const cTable = require('console.table');
+//const cTable = require('console.table');
 const mysql = require('mysql2');
 
 const connection = mysql.createConnection({
@@ -9,3 +9,54 @@ const connection = mysql.createConnection({
   password: process.env.PASS,
   database: process.env.DB,
 });
+connection.connect(err =>{
+  if (err) throw err;
+  console.log("Employee Tracker!");
+  menu();
+});
+
+const menu = () => {
+  inquirer.prompt ({
+    name: 'main',
+    type: 'list',
+    choices:[
+      'View all departments',
+      'View all jobs',
+      'View all employees',
+      'Add a department',
+      'Add a job',
+      'Add an employee',
+      'Update employee job',
+      'Exit',
+    ]
+  }).then(resp =>{
+    switch(resp.main){
+      case 'View all departments':
+        viewDepartment();
+        break;
+      case 'View all jobs':
+        viewJobs();
+        break;
+      case 'View all employees':
+        viewEmployees();
+        break;
+      case 'Add a department':
+        addDepartment();
+        break;
+      case 'Add a job':
+        addJob();
+        break;
+      case 'Add an employee':
+        addEmployee();
+        break;
+      case 'Update employee job':
+        updateEmployee();
+        break;
+      case "Exit":
+        connection.end();
+        break;
+      default:
+        connection.end();
+    }
+  });
+};
