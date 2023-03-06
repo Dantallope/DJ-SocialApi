8f099400fa46b770ac14507d6aeddb2e08441c89
Dantallope <dantebraccialini@gmail.com>
2023-03-06 16:30:02 -0700

    seed file

---
+++ db/seeds.sql
@@ -0,0 +1,16 @@
+insert into employees (first_name, last_name, role_id, manager_id)
+VALUES
+('Dante','Braccialini',1,null),
+('John','Doe',2,1),
+('Steve','Smith',3,1),
+('Chris','Hansen',3,1);
+insert into roles (title, salary,department_id)
+VALUES
+('Manager',1000,1),
+('HTML and CSS person',500,2),
+('Js person',800,3);
+insert into department(name)
+VALUES
+('Managment'),
+('FrontEnd'),
+('BackEnd');
