40dcd6c451b74fd9e35ce3263bcbf69ac5da3bbd
Dantallope <dantebraccialini@gmail.com>
2023-03-06 16:06:32 -0700

    schema has tables set up

--- db/schema.sql
+++ db/schema.sql
@@ -1,13 +1,25 @@
-drop database if exists employee_db;
-create database employee_db:
-use employee_db;
+drop database if exists tracker_db;
+create database tracker_db:
+use tracker_db;
+
+drop table if exists employee;
+drop table if exists role;
+drop table if exists department;
 
 create table employees (
   id int AUTO_INCREMENT not null,
-  firstName varchar(100) not null,
-  lastName varchar(100) not null,
-  title varchar(100) not null,
-  department varchar(100) not null,
-  salary int not null,
-  manager (varchar)
+  first_name varchar(30) not null,
+  last_name varchar(30) not null,
+  role_id int not null,
+  manager_id int not null
+)
+create table roles (
+  id int AUTO_INCREMENT not null,
+  title varchar(30) not  null,
+  salary decimal not null,
+  department_id int not null
+)
+create table department (
+  id int AUTO_INCREMENT not null,
+  name varchar(30)
 )
